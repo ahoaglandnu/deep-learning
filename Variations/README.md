abe4961dce811dab7c09f53b60acd802a6bf2592
Andy Hoagland <ahoaglandnu@users.noreply.github.com>
2018-02-25 22:42:54 -0500

    Add files via upload

---
+++ Variations/README.md
@@ -0,0 +1,48 @@
+# Toy neural network in numpy - variations
+
+Buidling off the original toy neural network
+
+# blobs.py
+
+### New
+
+![sklearn](http://scikit-learn.org/stable/_static/scikit-learn-logo-small.png)
+
+- [Generate isotropic Gaussian blobs]
+- [Standardize features by removing the mean and scaling to unit variance]
+- [Build a text report showing the main classification metrics]
+
+![matplotlib](https://matplotlib.org/_static/logo2.png)
+
+- [Make a scatter plot of x vs y]
+- [Make a scatter plot of x vs neural network output]
+
+
+[Generate isotropic Gaussian blobs]: http://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_blobs.html
+
+[Standardize features by removing the mean and scaling to unit variance]: http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
+
+[Build a text report showing the main classification metrics]: http://scikit-learn.org/stable/modules/generated/sklearn.metrics.classification_report.html
+
+[Make a scatter plot of x vs y]: https://matplotlib.org/api/_as_gen/matplotlib.pyplot.scatter.html#matplotlib.pyplot.scatter
+
+[Make a scatter plot of x vs neural network output]: https://matplotlib.org/api/_as_gen/matplotlib.pyplot.scatter.html#matplotlib.pyplot.scatter
+
+### Summary
+
+Randomly generated binary classification data set
+
+100 samples, 50 per class, 3 features
+
+Three hidden layers, tanh and sigmoid activation functions
+
+### Plot of data set
+
+![Figure_1](Figure_1.png) 
+
+### Expected Output
+
+![output_blobs](output_blobs.png)
+
+![Figure_2](Figure_2.png)
+
---
+++ Variations/blobs.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+plt.style.use('ggplot')
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+
+raw_X,y = make_blobs(centers=2, n_features=3, random_state=4321)
+
+plt.figure(figsize=(8, 8))
+plt.scatter(raw_X[:, 0], raw_X[:, 1], c=y, cmap='bwr')
+plt.title('Two Random Classes of Blobs')
+plt.show()
+
+# Set mean to 0 and stdev to 1
+X = StandardScaler().fit_transform(raw_X)
+y = np.reshape(y, (100,1)) 
+
+# activation functions
+def sigmoid(x,deriv=False):
+    if(deriv==True):
+        return x*(1-x)
+    return 1/(1+np.exp(-x))
+
+def tanh(x,deriv=False):
+    if(deriv==True):
+        z = np.tanh(x)
+        return 1 - (z**2)
+    return np.tanh(x)
+
+# randomize initial weights
+np.random.seed(1234)
+i_to_h1_weights = 2*np.random.random((3,100)) - 1
+h1_to_h2_weights = 2*np.random.random((100,100)) - 1
+h2_to_h3_weights = 2*np.random.random((100,100)) - 1
+h3_to_o_weights = 2*np.random.random((100,1)) - 1
+
+for j in range(10):
+
+    # forward pass
+    input_layer = X
+    hidden_layer_1 = tanh(np.dot(input_layer,i_to_h1_weights))
+    hidden_layer_2 = tanh(np.dot(hidden_layer_1,h1_to_h2_weights))
+    hidden_layer_3 = tanh(np.dot(hidden_layer_2,h2_to_h3_weights))
+    output_layer = sigmoid(np.dot(hidden_layer_3,h3_to_o_weights))
+
+    # track the change in error
+    output_layer_error = y - output_layer    
+    if j % 10 == 0:
+        print("Error based on random weights:" + str(np.mean(np.abs(output_layer_error))))
+    
+    # back propagate    
+    output_layer_delta = output_layer_error * sigmoid(output_layer,deriv=True)
+    
+    hidden_layer_3_error = output_layer_delta.dot(h3_to_o_weights.T)
+    hidden_layer_3_delta = hidden_layer_3_error * tanh(hidden_layer_3,deriv=True)
+
+    hidden_layer_2_error = hidden_layer_3_delta.dot(h2_to_h3_weights.T)
+    hidden_layer_2_delta = hidden_layer_2_error * tanh(hidden_layer_2,deriv=True)
+
+    hidden_layer_1_error = hidden_layer_2_delta.dot(h1_to_h2_weights.T)
+    hidden_layer_1_delta = hidden_layer_1_error * tanh(hidden_layer_1,deriv=True)
+
+    # update weights
+    h3_to_o_weights += hidden_layer_3.T.dot(output_layer_delta)
+    h2_to_h3_weights += hidden_layer_2.T.dot(hidden_layer_3_delta)
+    h1_to_h2_weights += hidden_layer_1.T.dot(hidden_layer_2_delta)
+    i_to_h1_weights += input_layer.T.dot(hidden_layer_1_delta)
+
+print("Trained network error:"+ str(np.mean(np.abs(output_layer_error))))
+y_true = np.reshape(y, (100,)) 
+y_pred = np.reshape(output_layer, (100,))
+y_pred = np.round(y_pred, decimals=0)
+print(classification_report(y_true, y_pred))
+plt.figure(figsize=(8, 8))
+plt.scatter(raw_X[:, 0], raw_X[:, 1], c=y_pred, cmap='winter')
+plt.title('Toy NN Results')
+plt.show()
